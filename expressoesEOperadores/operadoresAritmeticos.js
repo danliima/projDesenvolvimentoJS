68f525882aea40593e2918e1d2a53fcf603ea48a
Daniel Andrade <daniel9871994@gmail.com>
2023-01-02 20:40:40 -0400

    Aula de operadores aritméticos

---
+++ expressoesEOperadores/operadoresAritmeticos.js
@@ -0,0 +1,24 @@
+//Operadores Aritiméticos
+
+//multplicação
+//divisão
+//soma
+//subtração 
+
+
+//resto da divisão
+// let remaider
+// remaider = 11 % 9
+// console.log(remaider)
+
+//incremento
+// let increment = 0
+// increment++
+// console.log(increment)
+
+//decremento
+// let decrement = 0
+// console.log(--decrement)
+
+//exponencial
+console.log(3 ** 2)
