694db774c5cdd7d5bcecb1680d4d99bb235b9ba4
Daniel Andrade <daniel9871994@gmail.com>
2023-01-02 21:17:47 -0400

    Aula de operadores de atribuição

---
+++ expressoesEOperadores/operadoresDeAtribuicao.js
@@ -0,0 +1,24 @@
+// Operadores de Atribuição (Assignment)
+
+let x
+
+//Assignment
+x = 1
+
+//Addition assigment
+// x = x +2
+// x += 2
+
+//subtraction assigment
+// x -= 2
+
+//multiplicaction assigment
+// x *= 2
+
+//division assigment
+// x /= 2
+
+
+//remainder e exponetiation assigment
+x %= 2
+console.log(x)
