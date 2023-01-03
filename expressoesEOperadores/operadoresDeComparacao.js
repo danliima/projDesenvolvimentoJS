29089141f5c1b057995ac501195c9f1c3f56e52f
Daniel Andrade <daniel9871994@gmail.com>
2023-01-02 21:03:54 -0400

    Complementação da aula de comparação

--- expressoesEOperadores/operadoresDeComparacao.js
+++ expressoesEOperadores/operadoresDeComparacao.js
@@ -10,6 +10,16 @@ let two = 2
 // console.log(one == "1")
 
 // != diferente de 
-console.log(one != two)
-console.log(one != 1)
-console.log(one != "1")
+// console.log(one != two)
+// console.log(one != 1)
+// console.log(one != "1")
+
+//Existe também as expressões que comparam estritamente o valor
+
+// === estritamente igual a
+// console.log(one === 1)
+// console.log(one === "1")
+
+// !== estritamente diferente de 
+console.log(two !== "2")
+console.log(two !== 2)
