3f58dce336ad90c3abe7c7cf1ad4d7c6a5dd6637
Daniel Andrade <daniel9871994@gmail.com>
2023-01-04 21:12:59 -0400

    Entendendo as condicionais com operadores ternários

---
+++ expressoesEOperadores/operadorCondTernario.js
@@ -0,0 +1,23 @@
+// Operador condicional (ternário)
+
+// Dependendo da condição, nós recebemos valores diferentes
+
+//condição então valor 1 se não valor 2
+// condition ? valeu1 : value2
+
+//Exemplos
+//Café da manhã ótimo
+// let pao = false
+// let queijo = true
+
+// const niceBreakFast = pao || queijo ? 'Café da manhã ótimo':'Café da manhã ruim'
+
+
+
+//Maior de 18 anos
+
+let age = 15
+
+const canDrive = age >= 18 ? 'can drive':"can't drive"
+
+console.log(canDrive)
