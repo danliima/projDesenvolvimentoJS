f82cc9f0da4d963349613a8a11ae01eeb5ac0f9f
Daniel Andrade <daniel9871994@gmail.com>
2023-01-07 20:41:38 -0400

    Estrutura de repetição for of

---
+++ estruturaDeRepeticao/forOf.js
@@ -0,0 +1,17 @@
+//for...of
+
+//declaração que vai criar um loop através de uma variável
+
+
+//Pegando posições das letras da variável nome
+// let nome = 'Daniel'
+// for(let character of nome) {
+//     console.log(character)
+// }
+
+
+//Pegando a posição do Array
+let names = ['João', 'Manoel', 'Joana']
+for(let name of names) {
+    console.log(name)
+}
