45f2bbf78570f9a5442f4447ab5cbe399051c56a
Daniel Andrade <daniel9871994@gmail.com>
2023-01-07 20:31:03 -0400

    Estrutura de repetição while

---
+++ estruturaDeRepeticao/while.js
@@ -0,0 +1,18 @@
+//Estrutura de repetição 
+//while (enquanto)
+
+//while faz sentido nós usarmos quando não sabemos o momento de parar o loop
+
+// let i = 0;
+// while(i < 10){
+//     console.log(i)
+
+//     i++
+// }
+
+let i = 4655226598;
+while(i > 10){
+    console.log(i)
+
+    i /= 35
+}
