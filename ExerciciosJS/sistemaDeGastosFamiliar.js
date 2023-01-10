341c333ab271f407c406cdff21c8255744644c28
Daniel Andrade <daniel9871994@gmail.com>
2023-01-09 20:42:22 -0400

    Ajustando as casas decimais ni exercício

--- ExerciciosJS/sistemaDeGastosFamiliar.js
+++ ExerciciosJS/sistemaDeGastosFamiliar.js
@@ -9,7 +9,7 @@ Agora, crie uma função que irá calcular o total de receita e irá morstrar um
 
 let familyExpenses = {
     revenues: [2500, 1000, 2500],
-    expenses: [2500, 1000, 800, 700, 30]
+    expenses: [2500, 1000, 800.34, 700.33, 30.40]
 }
 
 //Criando a função que irá somar as receitas e despesas
@@ -39,7 +39,8 @@ function calculateBalance(){
         balanceText = 'Positivo'
     }
 
-    console.log(`Seu saldo é ${balanceText}: ${total} R$`)
+    //Imprimindo o resultado e ajustando as casas decimais com o toFixed
+    console.log(`Seu saldo é ${balanceText}: ${total.toFixed(2)} R$`)
 }
 
 calculateBalance()
